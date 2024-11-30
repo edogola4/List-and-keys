d69784856712b855da55b7908a78303786f49d02
edogola4 <edogola4@gmail.com>
2024-11-30 21:06:14 +0300

    List-and-Keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+    {/*<a href="home">home</a>
+  <a href="about">about</a>
+  <a href="projects">projects</a>*/}
+
+    {links.map((link) => (
+      <a href={`#${link}`} key={link}>
+        {link}
+      </a>
+    ))}
+  </nav>;
 }
 
 export default NavBar;
